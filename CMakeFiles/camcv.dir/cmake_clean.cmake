55969bb6203fa9666a38e8475564ab3ecac34d33
Martin Cai <martin_cai@hotmail.com>
2015-06-15 19:16:02 +0800

    first commit

---
+++ CMakeFiles/camcv.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/camcv.dir/RaspiCamControl.c.o"
+  "CMakeFiles/camcv.dir/RaspiCLI.c.o"
+  "CMakeFiles/camcv.dir/RaspiPreview.c.o"
+  "CMakeFiles/camcv.dir/camcv.c.o"
+  "CMakeFiles/camcv.dir/RaspiTex.c.o"
+  "CMakeFiles/camcv.dir/RaspiTexUtil.c.o"
+  "CMakeFiles/camcv.dir/tga.c.o"
+  "CMakeFiles/camcv.dir/gl_scenes/teapot.c.o"
+  "CMakeFiles/camcv.dir/gl_scenes/models.c.o"
+  "CMakeFiles/camcv.dir/gl_scenes/square.c.o"
+  "CMakeFiles/camcv.dir/gl_scenes/mirror.c.o"
+  "CMakeFiles/camcv.dir/gl_scenes/yuv.c.o"
+  "CMakeFiles/camcv.dir/gl_scenes/sobel.c.o"
+  "camcv.pdb"
+  "camcv"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/camcv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
